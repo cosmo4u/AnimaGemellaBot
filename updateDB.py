f41975d653d46c1d7b5b0243b920fc2b44b43235
joelepore <giuseppejoelepore@gmail.com>
2018-06-15 20:37:26 +0200

    Renamed the db to "anima_gemella.db"

--- updateDB.py
+++ updateDB.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('lovebot.db', check_same_thread=False)
+conn = sqlite3.connect('anima_gemella.db', check_same_thread=False)
 db = conn.cursor()
 
 def updateStep(step, chatid):
