72d281f2952de416b58c674cc58ef520495a913b
joelepore <giuseppejoelepore@gmail.com>
2018-06-16 19:30:12 +0200

    Deleted updateDB.py

--- updateDB.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('anima_gemella.db', check_same_thread=False)
-db = conn.cursor()
-
-def updateStep(step, chatid):
-    db.execute('UPDATE Persone SET Step = ? WHERE ID = ?', (step, chatid,))
-    conn.commit()
